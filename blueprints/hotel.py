3e543985519321a1dce37bad9ab0b941f48bf729
Roni <roni.physics@gmail.com>
2020-01-12 17:49:09 +0700

    Add hotel blueprints

--- blueprints/hotel.py
+++ blueprints/hotel.py
@@ -1,24 +1,30 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
-from services.sms import SMS
+'''
+REST API : Accessing MongoDB with pymongo
+'''
+from flask import Blueprint, request, jsonify, Response
+from appdb import db, ObjectId
+from bson.json_util import dumps
 
 hotelapi = Blueprint('hotel',__name__)
 
-@hotelapi.route('/hotel')
-def hotels():
-    
-    return 'List of hotels'
+@hotelapi.route('/hotel',methods=['GET'])
+def hotels():    
+    return Response(dumps(db.hotels.find()), content_type='application/json')
 
-@hotelapi.route('/hotel/<id>')
+@hotelapi.route('/hotel/<id>',methods=['GET'])
 def get_hotel_by_id(id):
-
-    SMS.send('62837482734','YEAAAYYYY')
-    return 'hotel: ' + id
+    return Response(dumps(db.hotels.find_one({'_id':ObjectId(id)}), content_type='application/json'))
 
 @hotelapi.route('/hotel/add', methods=['POST'])
 def add_hotel():
-    return 'a new hotel add'
+    hotel = request.get_json(force=True)
+    db.hotels.insert_one(hotel)
+    return Response(dumps(hotel), content_type='application/json')
 
-@hotelapi.route('/hotel/edit', methods=['POST'])
+@hotelapi.route('/hotel/update', methods=['POST'])
 def edit_hotel():
-    return 'Hotel edited'
+    hotel = request.get_json(force=True)
+    query = {"name": "My Everything"}
+    newvalues = {"$set":hotel}
+    db.hotels.update_one(query, newvalues)
+    return Response(dumps(hotel), content_type='application/json')
