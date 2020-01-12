d8a21f603c37f4ac085ddccb752e31eefefb2fff
Roni <roni.physics@gmail.com>
2020-01-12 17:49:28 +0700

    Add DB connection

---
+++ appdb.py
@@ -0,0 +1,7 @@
+import pymongo
+from bson.objectid import ObjectId
+from instance.config import MONGODB_CONNECTION_STRING
+
+db = pymongo.MongoClient(MONGODB_CONNECTION_STRING).flaskapp
+
+hotels = db.hotels
